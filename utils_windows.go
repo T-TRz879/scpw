211ad33ae2927cd9224ff8f9122f8cc95084f3bc
T-TRz879 <614560331@qq.com>
2023-06-07 22:49:43 +0800

    fix(utils_windows): support windows on stat time

---
+++ utils_windows.go
@@ -0,0 +1,14 @@
+package scpw
+
+import (
+	"os"
+	"strconv"
+	"syscall"
+)
+
+func StatTimeV2(file os.FileInfo) (string, string) {
+	stat := file.Sys().(*syscall.Win32FileAttributeData)
+	atime := time.Unix(0, stat.LastAccessTime.Nanoseconds()).Unix()
+	mtime := time.Unix(0, stat.LastWriteTime.Nanoseconds()).Unix()
+	return strconv.FormatInt(atime, 10), strconv.FormatInt(mtime, 10)
+}
